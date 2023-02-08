f14e6e11c23857bb34f14faf969afa0bf8a5a208
Lenz Lösche <lenzloesche@gmail.com>
2023-02-08 16:05:33 +0100

    solution by lenz

--- js/index.js
+++ js/index.js
@@ -2,3 +2,30 @@ console.clear();
 
 const form = document.querySelector('[data-js="form"]');
 const todoList = document.querySelector('[data-js="todoList"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+
+  const headline = data.headline;
+  const task = data.task;
+  console.log("Headline: " + headline + " Task: " + task);
+  createNewListItem(headline, task);
+  event.target.reset();
+  event.target.elements.headline.focus();
+});
+
+function createNewListItem(headline, task) {
+  const newListItem = document.createElement("li");
+  todoList.append(newListItem);
+
+  const newHeadline = document.createElement("h2");
+  newListItem.append(newHeadline);
+  newHeadline.textContent = headline;
+
+  const newTask = document.createElement("p");
+  newListItem.append(newTask);
+  newTask.textContent = task;
+}
